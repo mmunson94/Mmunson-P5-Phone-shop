571e4cff38439566ddffb3b3758467c17a5ef6f2
mmunson94 <michaeljackmunson9994@outlook.com>
2023-12-08 18:31:56 +0000

    Added images from Admin portal to templates

--- phoneshop_main/phoneshop_main/settings.py
+++ phoneshop_main/phoneshop_main/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static/']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
