1087b164eea95df930640a99f669317a36d71dfe
mmunson94 <michaeljackmunson9994@outlook.com>
2023-12-12 18:32:21 +0000

    Added signup page

--- phoneshop_main/website/views.py
+++ phoneshop_main/website/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Product
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 # Create your views here.
 def index(request):
@@ -24,7 +28,32 @@ def login_user(request):
     else:
         return render(request, 'login.html', {})
 
-def lougout_user(request):
+def logout_user(request):
     logout(request)
     messages.error(request, ("You have been logged out!"))
     return redirect('index')
+
+def register_user(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Register successful. Welcome ' + username + '!'))
+            return redirect('index')
+        else:
+            for field, errors in form.errors.items():
+                error_message = ', '.join(errors)
+                if field in ['password1', 'password2']:
+                    error_message += '. Password must contain at least 8 characters'
+                messages.error(request, f'{field.capitalize()}: {error_message}')
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
+    
