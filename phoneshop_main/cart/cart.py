80acff7009ce7f938d92c91efb880ee17a1c3917
mmunson94 <michaeljackmunson9994@outlook.com>
2023-12-21 19:31:34 +0000

    Cart summary page updated

--- phoneshop_main/cart/cart.py
+++ phoneshop_main/cart/cart.py
@@ -1,3 +1,5 @@
+from website.models import Product
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -21,3 +23,9 @@ class Cart():
 
     def __len__(self):
         return len(self.cart)
+
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        return products
--- phoneshop_main/cart/views.py
+++ phoneshop_main/cart/views.py
@@ -6,7 +6,9 @@ from django.http import JsonResponse
 
 # Create your views here
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    return render(request, 'cart_summary.html', {'cart_products': cart_products})
 
 def cart_add(request):
     print('inside the cart add function')
