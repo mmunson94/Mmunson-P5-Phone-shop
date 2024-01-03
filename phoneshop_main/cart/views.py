232c4904a2acd0be1b64fc4cc1ee6df71f7f0cee
mmunson94 <michaeljackmunson9994@outlook.com>
2024-01-03 19:29:37 +0000

    Quantity shows up in session object

--- phoneshop_main/cart/views.py
+++ phoneshop_main/cart/views.py
@@ -16,8 +16,9 @@ def cart_add(request):
 
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         cart_quantity = cart.__len__()
         response = JsonResponse({ 'qty': cart_quantity })
         return response
