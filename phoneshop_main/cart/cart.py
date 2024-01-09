51fbc1933d0684994dde6f3e4a7b39be357b91b9
mmunson94 <michaeljackmunson9994@outlook.com>
2024-01-09 19:26:32 +0000

    Added delete functionality to basket

--- phoneshop_main/cart/cart.py
+++ phoneshop_main/cart/cart.py
@@ -27,6 +27,25 @@ class Cart():
         # return len(self.cart)
         return sum(item for item in self.cart.values())
 
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        ourcart = self.cart
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+        thing = self.cart
+        return thing
+    
+    def delete(self, product):
+        product_id = str(product)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+        
+        self.session.modified = True
+
     def get_prods(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
@@ -36,3 +55,4 @@ class Cart():
     def get_quants(self):
         quantities = self.cart
         return quantities
+    
