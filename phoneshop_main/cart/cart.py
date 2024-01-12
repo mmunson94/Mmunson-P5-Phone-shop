3dfb777d13df12e208435f991887cc9ee0f58023
mmunson94 <michaeljackmunson9994@outlook.com>
2024-01-12 18:12:00 +0000

    Cart total displayed

--- phoneshop_main/cart/cart.py
+++ phoneshop_main/cart/cart.py
@@ -56,3 +56,20 @@ class Cart():
         quantities = self.cart
         return quantities
     
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+        total = 0
+
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+
+        return total
+
