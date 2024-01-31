88b7841e8cbd06022f34d7feb9b9dadb93fcea4a
mmunson94 <michaeljackmunson9994@outlook.com>
2024-01-31 19:56:37 +0000

    able to save subscribed user for newsletters

--- phoneshop_main/website/admin.py
+++ phoneshop_main/website/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Order, Product, Category
+from .models import Customer, Order, Product, Category, SubscribedUsers
 # Register your models here.
 
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(SubscribedUsers)
