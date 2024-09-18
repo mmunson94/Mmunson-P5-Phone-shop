39a829b23483ba3887498f8f8179b2d164f9b21d
mmunson94 <michaeljackmunson9994@outlook.com>
2024-09-18 18:31:02 +0100

    Session setup is done: Added cart app

---
+++ phoneshop_main/cart/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from website.models import Product
+from django.http import JsonResponse
+
+
+# Create your views here
+def cart_summary(request):
+    return render(request, 'cart_summary.html', {})
+
+def cart_add(request):
+    print('inside the cart add function')
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({ 'Product Name: ': product.name })
+        return response
+
+
+def cart_update(request):
+    pass
+
+def cart_delete(request):
+    pass
